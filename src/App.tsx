470f55279b4aed29d4b0e30686105b78d2a9ec39
Elio1995 <mehmetielio95@gmail.com>
2022-10-17 13:03:57 +0200

    Adding images in the main directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css'
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-function App() { 
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
+});
+function App() {
+  const classes = useStyles();
 
   return (
-    <div className="App">  
+    <div className="App">
+      <Button className={classes.root}>Hook</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
